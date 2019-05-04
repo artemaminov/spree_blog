2558019261d3c67556e50bccc0dece66192a880c
artemaminov <artemaminov@gmail.com>
2019-05-05 00:08:08 +0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
-  scope "blog", as: "blog" do
+  namespace :blog do
     resources :posts, only: [:index, :show]
   end
 
   namespace :admin do
-    scope "blog", as: "blog" do
+    namespace :blog do
       resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
     end
   end
