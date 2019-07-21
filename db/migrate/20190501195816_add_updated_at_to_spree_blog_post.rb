35218122fcd9687d5ecffd9c414872bf4a9b0188
Artem Aminov <artemaminov@gmail.com>
2019-07-21 16:39:50 +0300

    Migration inheritance from SpreeExtension

--- db/migrate/20190501195816_add_updated_at_to_spree_blog_post.rb
+++ db/migrate/20190501195816_add_updated_at_to_spree_blog_post.rb
@@ -1,4 +1,4 @@
-class AddUpdatedAtToSpreeBlogPost < ActiveRecord::Migration[5.2]
+class AddUpdatedAtToSpreeBlogPost < SpreeExtension::Migration[5.2]
   def change
     add_column :spree_blog_posts, :updated_at, :datetime
     add_column :spree_blog_posts, :created_at, :datetime
