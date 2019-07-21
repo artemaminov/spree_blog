397d8e454acb3d5272b1ea91e670dee9224e7c78
Artem Aminov <artemaminov@gmail.com>
2019-07-21 16:47:57 +0300

    add catalog link to navbar

---
+++ app/overrides/add_catalog_link_to_navbar.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:original => 'cc5e3ebef76fdc940c3f2c5c1e6a2f0c0224233c',
+                     :virtual_path => "spree/shared/_main_nav_bar",
+                     :name => "catalog_link",
+                     :insert_after => "#home-link",
+                     :partial => 'spree/blog/catalog_link',
+                     :disabled => false)
