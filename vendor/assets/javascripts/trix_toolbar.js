108928e4660a957f721c05eac713b901d7ccee6a
artemaminov <artemaminov@gmail.com>
2019-08-19 21:10:41 +0500

    add h2 h3 to editor

--- vendor/assets/javascripts/trix_toolbar.js
+++ vendor/assets/javascripts/trix_toolbar.js
@@ -1,54 +1,45 @@
-Trix.config.textAttributes.heading = { tagName: "h4", inheritable: false };
-Trix.config.textAttributes.sub = { tagName: "sub", inheritable: true };
+Trix.config.textAttributes.heading2 = { tagName: "h2", inheritable: true };
+Trix.config.textAttributes.heading3 = { tagName: "h3", inheritable: true };
+Trix.config.textAttributes.sup = { tagName: "sup", inheritable: true };
 Trix.config.textAttributes.sub = { tagName: "sub", inheritable: true };
 
 addEventListener("trix-initialize", function(event) {
-    let buttonHTML, buttonHTMLHeading, buttonGroup;
+    let element = event.target;
+    let editor = element.editor;
+    let toolbarElement = element.toolbarElement;
+    let groupElement = toolbarElement.querySelector(".trix-button-group--block-tools");
+    let headingElement = groupElement.querySelector(".trix-button--icon-heading-1");
 
-    buttonHTML  = '<button type="button" class="trix-button" data-trix-attribute="sup" title="Sup"><sup>SUP</sup></button>';
-    buttonHTML += '<button type="button" class="trix-button"data-trix-attribute="sub" title="Sub"><sub>SUB</sub></button>';
-    buttonHTMLHeading  = '<button type="button" class="trix-button"data-trix-attribute="heading" title="Heading">H</button>';
+    groupElement.insertAdjacentHTML("beforeend",'<button type="button" class="trix-button" data-trix-attribute="sup" title="Sup" tabindex="-1"><sup>SUP</sup></button>');
+    groupElement.insertAdjacentHTML("beforeend",'<button type="button" class="trix-button" data-trix-attribute="sub" title="Sub" tabindex="-1"><sub>SUB</sub></button>');
+    headingElement.insertAdjacentHTML("afterend",'<button type="button" class="trix-button" data-trix-attribute="heading3" title="Heading 3" tabindex="-1">H3</button>');
+    headingElement.insertAdjacentHTML("afterend",'<button type="button" class="trix-button" data-trix-attribute="heading2" title="Heading 2" tabindex="-1">H2</button>');
 
-    buttonGroup = event.target.toolbarElement.querySelector(".trix-button-group--text-tools");
-    buttonGroup.insertAdjacentHTML("beforeend", buttonHTML);
-    buttonGroup.insertAdjacentHTML("afterbegin", buttonHTMLHeading)
+    let selectedAttributes = new Set;
+    function updateSelectedAttributes() {
+        let selectedAttributes = new Set;
+        let selectedRange = editor.getSelectedRange();
+        if (selectedRange[0] === selectedRange[1]) { selectedRange[1]++ }
+
+        let selectedPieces = editor.getDocument().getDocumentAtRange(selectedRange).getPieces();
+        selectedPieces.forEach(function(piece) {
+            Object.keys(piece.getAttributes()).forEach(function(attribute) {
+                selectedAttributes.add(attribute);
+            })
+        });
+    }
+
+    function enforceExclusiveAttributes() {
+        if (editor.attributeIsActive("sup") && selectedAttributes.has("sub")) {
+            updateSelectedAttributes();
+            editor.deactivateAttribute("sub");
+        } else if (editor.attributeIsActive("sub") && selectedAttributes.has("sup")) {
+            updateSelectedAttributes();
+            editor.deactivateAttribute("sup");
+        }
+    }
+
+    updateSelectedAttributes();
+    element.addEventListener("trix-selection-change", updateSelectedAttributes);
+    element.addEventListener("trix-change", enforceExclusiveAttributes);
 });
-
-// addEventListener("trix-initialize", function(event) {
-//     var element = event.target
-//     var editor = element.editor
-//     var toolbarElement = element.toolbarElement
-//     var groupElement = toolbarElement.querySelector(".button_group.text_tools")
-//
-//     groupElement.insertAdjacentHTML("beforeend", '<button type="button" data-trix-attribute="sup"><sup>SUP</sup></button>')
-//     groupElement.insertAdjacentHTML("beforeend",'<button type="button" data-trix-attribute="sub"><sub>SUB</sub></button>')
-//
-//     var selectedAttributes = new Set
-//     function updateSelectedAttributes() {
-//         selectedAttributes = new Set
-//
-//         var selectedRange = editor.getSelectedRange()
-//         if (selectedRange[0] === selectedRange[1]) { selectedRange[1]++ }
-//
-//         var selectedPieces = editor.getDocument().getDocumentAtRange(selectedRange).getPieces()
-//         selectedPieces.forEach(function(piece) {
-//             Object.keys(piece.getAttributes()).forEach(function(attribute) {
-//                 selectedAttributes.add(attribute)
-//             })
-//         })
-//     }
-//
-//     function enforceExclusiveAttributes() {
-//         if (editor.attributeIsActive("sup") && selectedAttributes.has("sub")) {
-//             editor.deactivateAttribute("sub")
-//             updateSelectedAttributes()
-//         } else if (editor.attributeIsActive("sub") && selectedAttributes.has("sup")) {
-//             editor.deactivateAttribute("sup")
-//             updateSelectedAttributes()
-//         }
-//     }
-//
-//     updateSelectedAttributes()
-//     element.addEventListener("trix-selection-change", updateSelectedAttributes)
-//     element.addEventListener("trix-change", enforceExclusiveAttributes)
-// })
